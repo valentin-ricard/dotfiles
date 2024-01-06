a388d3abea3a715d0f16aa3f8d1c975a5aa1ac2a
Valentin Ricard <91870700+valentin-ricard@users.noreply.github.com>
2024-01-06 20:19:44 +0100

    chore: Fixed device not entering sleep

--- dot_config/hypr/scripts/executable_sleep.sh
+++ dot_config/hypr/scripts/executable_sleep.sh
@@ -1,6 +1,6 @@
 LOCK_TIME=180
 SUSPEND_TIME=300
 
-swayidle -w timeout $LOCK_TIME 'gtklock' \
+swayidle -w timeout $LOCK_TIME 'swaylock -f' \
             timeout $SUSPEND_TIME 'systemctl suspend' \
-            before-sleep 'gtklock' &
+            before-sleep 'swaylock -f' &
